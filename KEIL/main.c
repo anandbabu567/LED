5b21021efaeb165bd20a5d8cf7bfc1287cb9c687
AnandBabu <chanandbabu2018.com>
2023-12-14 23:03:16 +0530

    added files for project

--- KEIL/main.c
+++ KEIL/main.c
@@ -2,17 +2,22 @@
 #include "types.h"
 #include "defines.h"
 #include "delay.h"
-#define LED 7
+#define LED 7			// Led connect to P0.7
+#define SW 0			// Led connect to Switch P0.0
 
 int main()
 {
 	IODIR0 = 1<<LED;
 	while(1)
 	{
-		IOSET0 =1<<LED;
-		delay_ms(1000);
-		IOCLR0 = 1<<LED;
-		delay_ms(1000);
+		if (((IOPIN0 >> SW)&1) == 0)
+		{
+			IOPIN0|= 1<<LED;
+		}
+		else
+		{
+			IOPIN0&= 1<<LED;
+		}
 	}
 }
 
